97a6186d6e2481b9ae96c273c2fd488dd6ac6471
manuo1 <manuo1@free.fr>
2021-02-17 20:37:36 +0100

    New function new personal info page

--- app_users/urls.py
+++ app_users/urls.py
@@ -5,14 +5,14 @@ from app_products.forms import ProductSearchForm
 from app_users import views
 
 urlpatterns = [
-    path('login/', views.loginPage, name='loginPage'),
-    path('register/', views.registerPage, name='registerPage'),
-    path('logout/', views.logoutCurrentUser, name='logoutCurrentUser'),
-    path('profile/', views.profile, name='profilePage'),
-    path('personal-information/', views.userInfoPage, name='userInfoPage'),
-    path('update-password/', auth_views.PasswordChangeView.as_view(
+    path('login', views.loginPage, name='loginPage'),
+    path('register', views.registerPage, name='registerPage'),
+    path('logout', views.logoutCurrentUser, name='logoutCurrentUser'),
+    path('profile', views.profile, name='profilePage'),
+    path('personal-information', views.userInfoPage, name='userInfoPage'),
+    path('update-password', auth_views.PasswordChangeView.as_view(
         template_name='update-password.html',
-        success_url='userInfoPage',
+        success_url='personal-information',
         extra_context={'search_form': ProductSearchForm()},
         ),
         name='update-password'
