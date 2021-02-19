7c6eac6dfdac4d50772b8ab9c21fccc261ed2779
manuo1 <manuo1@free.fr>
2021-02-19 12:49:02 +0100

    refactoring app_users functional tests

--- app_users/tests/functionnal/test_firefox.py
+++ app_users/tests/functionnal/test_firefox.py
@@ -17,7 +17,6 @@ class FirefoxFunctionalTestCases(LiveServerTestCase):
         cls.driver.implicitly_wait(30)
         cls.driver.maximize_window()
 
-
     @classmethod
     def tearDownClass(cls):
         super().tearDownClass()
@@ -33,113 +32,92 @@ class FirefoxFunctionalTestCases(LiveServerTestCase):
             email="testusername@mail.com",
         )
 
-
     def test_user_can_connect_and_disconnect(self):
         """test if user can connect and disconnect."""
-        self.driver.get(self.live_server_url)
-        self.driver.find_element_by_id('button-login').click()
-        self.driver.find_element_by_id('id_username').send_keys("testusername")
-        self.driver.find_element_by_id('id_password').send_keys("testpassword")
-        self.driver.find_element_by_id('button-login-submit').click()
-        self.driver.find_element_by_id('button-logout').click()
+        self.go_to_url_name('indexPage')
+        self.login_the_user ()
+        self.click_on_id('button-logout')
         self.assertTrue(self.driver.find_element_by_id('button-login'))
 
     def test_user_can_display_his_profile(self):
         """test if user can display his profil."""
-        self.driver.get(self.live_server_url)
-        self.driver.find_element_by_id('button-login').click()
-        self.driver.find_element_by_id('id_username').send_keys("testusername")
-        self.driver.find_element_by_id('id_password').send_keys("testpassword")
-        self.driver.find_element_by_id('button-login-submit').click()
-        self.driver.find_element_by_id('button-profile').click()
-        email = self.driver.find_element_by_id('profile-mail').get_attribute(
-            'innerHTML'
-        )
-        self.assertEqual(email, " testusername@mail.com ")
-
-    def test_user_can_create_an_account(self):
-        """test if user can create an account."""
-        self.driver.get(self.live_server_url)
-        self.driver.find_element_by_id('button-login').click()
-        self.driver.find_element_by_id('button-create-account').click()
-        self.driver.find_element_by_id('id_username').send_keys(
-            "testusername2"
-        )
-        self.driver.find_element_by_id('id_first_name').send_keys(
-            "testuserfirstname2"
-        )
-        self.driver.find_element_by_id('id_email').send_keys(
-            "testusername2@mail.com"
-        )
-        self.driver.find_element_by_id('id_password1').send_keys(
-            "testpassword2"
-        )
-        self.driver.find_element_by_id('id_password2').send_keys(
-            "testpassword2"
-        )
-        self.driver.find_element_by_id('button-create-submit').click()
-        message = self.driver.find_element_by_id(
-            'login-messages'
-        ).get_attribute('innerHTML')
-        self.assertEqual(
-            message, " Un nouveau compte vient d'être créé pour testusername2 "
-        )
-
-    def test_user_can_update_his_personnal_info(self):
-        """test if user can update his personnals informations."""
-        self.user_login()
-        self.driver.get(self.live_server_url + reverse('updateUserInfoPage'))
-        """find form fields"""
-        username_field = self.driver.find_element_by_id(
-            'id_username_update')
-        first_name_field = self.driver.find_element_by_id(
-            'id_first_name_update')
-        email_field = self.driver.find_element_by_id(
-            'id_email_update')
-        """clear form fields """
-        username_field.clear()
-        first_name_field.clear()
-        email_field.clear()
-        """ fill form fields """
-        username_field.send_keys("testusername3")
-        first_name_field.send_keys("testfirstname3")
-        email_field.send_keys("testusername3@mail.com")
-        self.driver.find_element_by_id(
-            'button-user-info-update-submit').click()
-        messages = self.driver.find_element_by_id('messages'
-            ).get_attribute('innerHTML')
-        self.assertTrue(
-            "Votre nom d'utilisateur a été modifié" in messages
-            and
-            "Votre prénom a été modifié" in messages
-            and
-            "Votre adresse email a été modifié" in messages)
-
-    def test_user_can_update_his_password(self):
-        """test if user can update his personnals informations."""
-        self.user_login()
-        self.driver.get(self.live_server_url + reverse('update-password'))
-        self.driver.find_element_by_id('id_old_password').send_keys(
-            'testpassword'
-        )
-        self.driver.find_element_by_id('id_new_password1').send_keys(
-            'New_testpassword'
-        )
-        self.driver.find_element_by_id('id_new_password2').send_keys(
-            'New_testpassword'
-        )
-        self.driver.find_element_by_id('button-psw-update-submit').click()
-        self.driver.get(self.live_server_url + reverse('logoutCurrentUser'))
-        self.user_login(password='New_testpassword')
+        self.go_to_url_name('indexPage')
+        self.login_the_user ()
+        self.click_on_id('button-profile')
         self.assertEqual(
             self.driver.current_url,
             self.live_server_url + reverse('profilePage')
         )
 
-    def user_login(self, username='testusername', password='testpassword'):
+    def test_user_can_create_an_account(self):
+        """test if user can create an account."""
+        self.go_to_url_name('indexPage')
+        self.click_on_id('button-login')
+        self.click_on_id('button-create-account')
+        self.write_in_id('id_username', "testusername2")
+        self.write_in_id('id_first_name', "testuserfirstname2")
+        self.write_in_id('id_email', "testusername2@mail.com")
+        self.write_in_id('id_password1', "testpassword2")
+        self.write_in_id('id_password2', "testpassword2")
+        self.click_on_id('button-create-submit')
+        self.assertTrue(
+            "Un nouveau compte vient d'être créé pour testusername2"
+            in
+            self.get_html_in('login-messages')
+        )
+
+    def test_user_can_update_his_personnal_info(self):
+        """test if user can update his personnals informations."""
+        self.login_the_user ()
+        self.go_to_url_name('updateUserInfoPage')
+        self.write_in_id('id_username_update', 'testusername3')
+        self.write_in_id('id_first_name_update', 'testfirstname3')
+        self.write_in_id('id_email_update', 'testusername3@mail.com')
+        self.click_on_id('button-user-info-update-submit')
+        confirmation_messages = self.get_html_in('messages')
+        self.assertTrue(
+            "Votre nom d'utilisateur a été modifié" in confirmation_messages
+            and
+            "Votre prénom a été modifié" in confirmation_messages
+            and
+            "Votre adresse email a été modifié" in confirmation_messages
+        )
+
+    def test_user_can_update_his_password(self):
+        """test if user can update his personnals informations."""
+        self.login_the_user ()
+        self.go_to_url_name('update-password')
+        self.write_in_id('id_old_password', 'testpassword')
+        self.write_in_id('id_new_password1', 'New_testpassword')
+        self.write_in_id('id_new_password2', 'New_testpassword')
+        self.click_on_id('button-psw-update-submit')
+        self.go_to_url_name('logoutCurrentUser')
+        self.login_the_user (password='New_testpassword')
+        self.assertEqual(
+            self.driver.current_url,
+            self.live_server_url + reverse('profilePage')
+        )
+
+    def login_the_user(self, username='testusername', password='testpassword'):
         """test if user can connect and disconnect."""
-        self.driver.get(self.live_server_url)
-        self.driver.find_element_by_id('button-login').click()
-        self.driver.find_element_by_id('id_username').send_keys(username)
-        self.driver.find_element_by_id('id_password').send_keys(password)
-        self.driver.find_element_by_id('button-login-submit').click()
+        self.go_to_url_name('indexPage')
+        self.click_on_id('button-login')
+        self.write_in_id('id_username', username)
+        self.write_in_id('id_password', password)
+        self.click_on_id('button-login-submit')
+
+    def write_in_id(self, element_id, value):
+        element = self.driver.find_element_by_id(element_id)
+        element.clear()
+        element.send_keys(value)
+
+    def click_on_id(self, element_id):
+        self.driver.find_element_by_id(element_id).click()
+
+    def get_html_in(self, element_id):
+        html = self.driver.find_element_by_id(element_id
+            ).get_attribute('innerHTML')
+        return html
+
+    def go_to_url_name(self, url_name):
+        self.driver.get(self.live_server_url + reverse(url_name))
